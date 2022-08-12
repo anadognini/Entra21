91fadcb14289030f9e1585b67db9ef2498a80596
Ana <92342881+anadognini@users.noreply.github.com>
2022-08-12 19:24:10 -0300

    Update ProjetoPesquisa.java

--- POO/Lista_11_POO/ProjetoPesquisa.java
+++ POO/Lista_11_POO/ProjetoPesquisa.java
@@ -4,128 +4,141 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ProjetoPesquisa {
-	// Atributos
-	private String codigo;
-	private String titulo;
-	private LocalDate dataInicio;
-	private LocalDate dataFim;
-	private String profResponsavel;
-	private List<Pesquisador> equipe = new ArrayList<Pesquisador>();
-	
-	// Construtor
-	
-	
-	// Getters e setters
-	public String getCodigo() {
-		return codigo;
-	}
-	
-	public ProjetoPesquisa(Scanner entradas) {
-		// Setando o código
-		System.out.println("Informe o código do projeto: ");
-		String codigo = entradas.next();
-		
-		setCodigo(codigo);
-		
-		// Setando o título
-		System.out.println("Informe o título do projeto: ");
-		String titulo = entradas.next();
-		
-		setTitulo(titulo);
-		
-		// Setando a data de início
-		System.out.println("Informe o dia, mês e ano de início do projeto, nessa ordem."
-				+ "\nDia:");
-		int diaInicio = entradas.nextInt();
-		System.out.println("Mês: ");
-		int mesInicio = entradas.nextInt();
-		System.out.println("Ano: ");
-		int anoInicio = entradas.nextInt();
-		
-		LocalDate dataInicio = LocalDate.of(anoInicio, mesInicio, diaInicio);
-		
-		setDataInicio(dataInicio);
-		
-		// Setando a data de fim
-		System.out.println("Informe o dia, mês e ano de término do projeto, nessa ordem:");
-		int diaFim = entradas.nextInt();
-		System.out.println("Mês: ");
-		int mesFim = entradas.nextInt();
-		System.out.println("Ano: ");
-		int anoFim = entradas.nextInt();
-		
-		LocalDate dataFim = LocalDate.of(anoFim, mesFim, diaFim);
-		
-		setDataFim(dataFim);
-		
-		// Setando o professor responsável
-		System.out.println("Informe o nome do professor responsável (é importante que seja um dos professores da lista): ");
-		Professor opcaoProfessor = new Professor();
-		
-		String[] nomesProfessores = opcaoProfessor.getNomesProfessores();
-		
-		nomesProfessores[0] = "Fernando";
-		nomesProfessores[1] =  "Carlos";
-		nomesProfessores[2] = "Andrea";
-		
-		for (int i = 0; i < 3; i++) {
-			System.out.println(nomesProfessores[i]);
-		}
-	
-		String profResponsavel = entradas.next();
-		
-		for (int i = 0; i < 3; i++) {
-			if (!profResponsavel.equals(nomesProfessores[i])) {
-				System.out.println("O nome informado não consta na lista, favor inserir novamente: ");
-				profResponsavel = entradas.next();
-			} else {
-				setProfResponsavel(profResponsavel);				
-			}
-		}
-		
-	}
+    // Atributos
+    private String codigo;
+    private String titulo;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private String profResponsavel;
+    private List<Pesquisador> equipe = new ArrayList<Pesquisador>();
 
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-	
-	public String getTitulo() {
-		return titulo;
-	}
-	
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	
-	public LocalDate getDataInicio() {
-		return dataInicio;
-	}
-	
-	public void setDataInicio(LocalDate dataInicio) {
-		this.dataInicio = dataInicio;
-	}
-	
-	public LocalDate getDataFim() {
-		return dataFim;
-	}
-	
-	public void setDataFim(LocalDate dataFim) {
-		this.dataFim = dataFim;
-	}
-	
-	public String getProfResponsavel() {
-		return profResponsavel;
-	}
-	
-	public void setProfResponsavel(String profResponsavel) {
-		this.profResponsavel = profResponsavel;
-	}
-	
-	public List<Pesquisador> getEquipe() {
-		return equipe;
-	}
-	
-	public void setEquipe(List<Pesquisador> equipe) {
-		this.equipe = equipe;
-	}
+    // Construtor
+    public ProjetoPesquisa(Scanner entradas) {
+        // Setando o cÃ³digo
+        setCodigo(codigo, entradas);
+
+        // Setando o tÃ­tulo
+        setTitulo(titulo, entradas);
+
+        // Setando a data de inÃ­cio
+        setDataInicio(dataInicio, entradas);
+
+        // Setando a data de fim
+        setDataFim(dataFim, entradas);
+
+        // Setando o professor responsÃ¡vel
+        setProfResponsavel(profResponsavel, entradas);
+    }
+
+    // Getters e setters
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigoProjeto, Scanner entradas) {
+        System.out.println("Informe o cÃ³digo do projeto: ");
+        codigoProjeto = entradas.next();
+
+        this.codigo = codigoProjeto;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String tituloProjeto, Scanner entradas) {
+        System.out.println("Informe o tÃ­tulo do projeto: ");
+        tituloProjeto = entradas.next();
+
+        this.titulo = tituloProjeto;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicioProjeto, Scanner entradas) {
+        System.out.println("Informe o dia, mÃªs e ano de inÃ­cio do projeto, nessa ordem."
+                + "\nDia:");
+        int diaInicio = entradas.nextInt();
+
+        System.out.println("MÃªs: ");
+        int mesInicio = entradas.nextInt();
+
+        System.out.println("Ano: ");
+        int anoInicio = entradas.nextInt();
+
+        dataInicioProjeto = LocalDate.of(anoInicio, mesInicio, diaInicio);
+
+        this.dataInicio = dataInicioProjeto;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFimProjeto, Scanner entradas) {
+        System.out.println("Informe o dia, mÃªs e ano de tÃ©rmino do projeto, nessa ordem."
+                + "\nDia:");
+        int diaFim = entradas.nextInt();
+
+        System.out.println("MÃªs: ");
+        int mesFim = entradas.nextInt();
+
+        System.out.println("Ano: ");
+        int anoFim = entradas.nextInt();
+
+        dataFimProjeto = LocalDate.of(anoFim, mesFim, diaFim);
+
+        this.dataFim = dataFimProjeto;
+    }
+
+    public String getProfResponsavel() {
+        return profResponsavel;
+    }
+
+    public void setProfResponsavel(String profResponsavelProjeto, Scanner entradas) {
+        System.out.println(
+                "Informe o nome do professor responsÃ¡vel (Ã© importante que seja um dos professores da lista): ");
+
+        Professor opcaoProfessor = new Professor();
+
+        String[] nomesProfessores = opcaoProfessor.getNomesProfessores();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(nomesProfessores[i]);
+        }
+
+        profResponsavelProjeto = entradas.next();
+
+        boolean flag = verificarProf(profResponsavelProjeto, entradas, nomesProfessores);
+
+        if (flag != true) {
+            System.out.println("O nome informado nÃ£o consta na lista. Favor informar outro nome.");
+            profResponsavelProjeto = entradas.next();
+        } else {
+            this.profResponsavel = profResponsavelProjeto;
+        }
+    }
+
+    public List<Pesquisador> getEquipe() {
+        return equipe;
+    }
+
+    public void setEquipe(List<Pesquisador> equipe) {
+        this.equipe = equipe;
+    }
+
+    private boolean verificarProf(String profResponsavelProjeto, Scanner entradas, String[] nomesProfessores) {
+        boolean flag;
+
+        if (profResponsavelProjeto.equals("Fernando") || profResponsavelProjeto.equals("Carlos")
+                || profResponsavelProjeto.equals("Andrea")) {
+            flag = true;
+        } else {
+            flag = false;
+        }
+
+        return flag;
+    }
 }
