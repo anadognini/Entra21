cf4ea3f8fd3291f3705340ec3d0f6b85a7aaa77c
Ana <92342881+anadognini@users.noreply.github.com>
2022-08-13 20:51:44 -0300

    Update Teste.java

--- POO/Lista_11_POO/Teste.java
+++ POO/Lista_11_POO/Teste.java
@@ -2,62 +2,61 @@ import java.util.Scanner;
 
 public class Teste {
 
-	public static void main(String[] args) {
-		Scanner entradas = new Scanner(System.in);
-		
-		do { 
-			int opcao;
-			boolean flag;
-			
-			System.out.println("Escolha a opÁ„o: \n1 - " + Menu.CADASTRAR_PROJETO
-					+ "\n2 - " + Menu.INSERIR_PROF
-					+ "\n3 - " + Menu.INSERIR_ALUNOS
-					+ "\n4 - " + Menu.BUSCAR_POR_CODIGO
-					+ "\n0 - " + Menu.SAIR);
-			
-			opcao = entradas.nextInt();
-			flag = criarMenu(opcao, entradas);
-			
-			if (flag == false) {
-				break;
-			}
-			
-		} while (true);
-		
-//		entradas.close();
+    public static void main(String[] args) {
+        Scanner entradas = new Scanner(System.in);
 
-	}
+        do {
+            int opcao;
+            boolean flag;
 
-	public static boolean criarMenu(int opcao, Scanner entradas) {
-		boolean t = true;
+            System.out.println("Escolha a op√ß√£o: \n1 - " + Menu.CADASTRAR_PROJETO
+                    + "\n2 - " + Menu.INSERIR_PROF
+                    + "\n3 - " + Menu.INSERIR_ALUNOS
+                    + "\n4 - " + Menu.BUSCAR_POR_CODIGO
+                    + "\n0 - " + Menu.SAIR);
 
-		switch (opcao) {
-			case 1:
-				ProjetoPesquisa projeto1 = new ProjetoPesquisa (entradas);
-				break;
+            opcao = entradas.nextInt();
+            flag = criarMenu(opcao, entradas);
 
-			case 2:
-				System.out.println("Oi");
-				break;
+            if (flag == false) {
+                break;
+            }
 
-			case 3:
-				System.out.println("Oi");
-				break;
+        } while (true);
 
-			case 4:
-				System.out.println("Oi");
-				break;
+        // entradas.close();
 
-			case 0:
-				t = false;
-				
-			default:
-				if (opcao != 0) {					
-					System.out.println("A opÁ„o digitada n„o È v·lida. Digite novamente a opÁ„o que deseja.");
-				}
-		}
+    }
 
-		return t;
-	}
-	
+    public static boolean criarMenu(int opcao, Scanner entradas) {
+        boolean t = true;
+
+        switch (opcao) {
+            case 1:
+                ProjetoPesquisa projeto1 = new ProjetoPesquisa(entradas);
+                break;
+
+            case 2:
+                Professor professor1 = new Professor(entradas);
+                break;
+
+            case 3:
+                System.out.println("Oi");
+                break;
+
+            case 4:
+                System.out.println("Oi");
+                break;
+
+            case 0:
+                t = false;
+
+            default:
+                if (opcao != 0) {
+                    System.out.println("A op√ß√£o digitada n√£o √© v√°lida. Digite novamente a op√ß√£o que deseja.");
+                }
+        }
+
+        return t;
+    }
 }
